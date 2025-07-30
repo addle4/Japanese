8cb933b73176e2e66ec6cac165b14104ad702508
KimSehan <dosacha@naver.com>
2025-07-30 14:35:57 +0900

    Initial Commit

---
+++ 일본어/________App.swift
@@ -0,0 +1,17 @@
+//
+//  ________App.swift
+//  일본어
+//
+//  Created by dosacha on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
